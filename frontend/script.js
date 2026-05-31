4969b7d6e296d57d0137b0d49fd36904bb75a532
Alan Miranda <154455950+alanmiranda-dev@users.noreply.github.com>
2026-05-30 23:58:45 -0300

    Criacao do html + css + js

---
+++ frontend/script.js
@@ -0,0 +1,52 @@
+const API_URL = 'http://localhost:3000/produtos';
+
+async function carregarProdutos() {
+
+    const resposta = await fetch(API_URL);
+
+    const produtos = await resposta.json();
+
+    const tabela = document.getElementById('tabelaProdutos');
+
+    tabela.innerHTML = '';
+
+    produtos.forEach(produto => {
+
+        tabela.innerHTML += `
+            <tr>
+                <td>${produto.id}</td>
+                <td>${produto.nome}</td>
+                <td>${produto.quantidade}</td>
+                <td>R$ ${produto.preco}</td>
+            </tr>
+        `;
+
+    });
+}
+
+document
+    .getElementById('produtoForm')
+    .addEventListener('submit', async (e) => {
+
+        e.preventDefault();
+
+        const produto = {
+            nome: document.getElementById('nome').value,
+            quantidade: Number(document.getElementById('quantidade').value),
+            preco: Number(document.getElementById('preco').value)
+        };
+
+        await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(produto)
+        });
+
+        e.target.reset();
+
+        carregarProdutos();
+    });
+
+carregarProdutos();
