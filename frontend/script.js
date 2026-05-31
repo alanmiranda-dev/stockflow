dcbe4c577a8eb9ed54b2e7217a23e95736671ae2
Alan Miranda <154455950+alanmiranda-dev@users.noreply.github.com>
2026-05-31 01:10:17 -0300

    atualizando index.html

--- frontend/script.js
+++ frontend/script.js
@@ -10,18 +10,27 @@ async function carregarProdutos() {
 
     tabela.innerHTML = '';
 
-    produtos.forEach(produto => {
+   produtos.forEach(produto => {
 
-        tabela.innerHTML += `
-            <tr>
-                <td>${produto.id}</td>
-                <td>${produto.nome}</td>
-                <td>${produto.quantidade}</td>
-                <td>R$ ${produto.preco}</td>
-            </tr>
-        `;
+    tabela.innerHTML += `
+        <tr>
+            <td>${produto.id}</td>
+            <td>${produto.nome}</td>
+            <td>${produto.quantidade}</td>
+            <td>R$ ${produto.preco}</td>
+            <td>
+                <button onclick="editarProduto(${produto.id})">
+                    Editar
+                </button>
 
-    });
+                <button onclick="excluirProduto(${produto.id})">
+                    Excluir
+                </button>
+            </td>
+        </tr>
+    `;
+
+});
 }
 
 document
@@ -50,3 +59,51 @@ document
     });
 
 carregarProdutos();
+
+async function excluirProduto(id) {
+
+    const confirmar = confirm(
+        'Deseja realmente excluir este produto?'
+    );
+
+    if (!confirmar) {
+        return;
+    }
+
+    await fetch(`${API_URL}/${id}`, {
+        method: 'DELETE'
+    });
+
+    carregarProdutos();
+}
+
+async function editarProduto(id) {
+
+    const nome = prompt('Novo nome:');
+
+    const quantidade = prompt('Nova quantidade:');
+
+    const preco = prompt('Novo preço:');
+
+    if (!nome || !quantidade || !preco) {
+        return;
+    }
+
+    await fetch(`${API_URL}/${id}`, {
+
+        method: 'PUT',
+
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            nome,
+            quantidade: Number(quantidade),
+            preco: Number(preco)
+        })
+
+    });
+
+    carregarProdutos();
+}
